9e941611a19ad5006d1d42aa8a73127a45687d8b
Jeff McLean <44034094+maxjeffos@users.noreply.github.com>
2020-12-08 21:29:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "running 'node dist/index.js'"
 node dist/index.js
 
-echo "\nrunning 'node packages/mod-cli-spike/some-top-level-command/dist/index.js'"
-node packages/mod-cli-spike/some-top-level-command/dist/index.js
+echo "\nrunning 'node packages/mod-cli-spike-some-top-level-command/dist/index.js'"
+node packages/mod-cli-spike-some-top-level-command/dist/index.js
