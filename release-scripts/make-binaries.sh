f4debb7881440206af6e2a68801b66627af70089
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2020-12-17 16:57:49 -0500

    chore: save binaries to folder and artifact them during canary

--- release-scripts/make-binaries.sh
+++ release-scripts/make-binaries.sh
@@ -2,13 +2,15 @@
 set -e
 echo "TODO: Make Binaries"
 
+mkdir binary-releases
 echo '' > dist/STANDALONE
-npx pkg . -t node14-alpine-x64 -o mod-cli-spike-alpine
-npx pkg . -t node12-linux-x64 -o mod-cli-spike-linux
-npx pkg . -t node12-macos-x64 -o mod-cli-spike-macos
-npx pkg . -t node12-win-x64 -o mod-cli-spike-win.exe
+npx pkg . -t node14-alpine-x64 -o binary-releases/mod-cli-spike-alpine
+npx pkg . -t node12-linux-x64 -o binary-releases/mod-cli-spike-linux
+npx pkg . -t node12-macos-x64 -o binary-releases/mod-cli-spike-macos
+npx pkg . -t node12-win-x64 -o binary-releases/mod-cli-spike-win.exe
 # TODO: build docker release
 # TODO: sign win binary
 # TODO: shasum for win?
-shasum -a 256 mod-cli-spike-linux > mod-cli-spike-linux.sha256 && shasum -a 256 mod-cli-spike-macos > mod-cli-spike-macos.sha256 && shasum -a 256 mod-cli-spike-alpine > mod-cli-spike-alpine.sha256
-ls -la
+shasum -a 256 binary-releases/mod-cli-spike-linux > binary-releases/mod-cli-spike-linux.sha256
+shasum -a 256 binary-releases/mod-cli-spike-macos > binary-releases/mod-cli-spike-macos.sha256
+shasum -a 256 binary-releases/mod-cli-spike-alpine > binary-releases/mod-cli-spike-alpine.sha256
